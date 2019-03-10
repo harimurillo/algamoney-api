788287d60f0029dc7cbca4e4aca99efdad4a82fe
harimurillo <harimurillo@gmail.com>
2019-03-09 22:42:40 -0300

    5.6 OK

--- src/main/java/com/example/algamoney/api/service/LancamentoService.java
+++ src/main/java/com/example/algamoney/api/service/LancamentoService.java
@@ -6,13 +6,19 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.example.algamoney.api.model.Lancamento;
+import com.example.algamoney.api.model.Pessoa;
 import com.example.algamoney.api.repository.LancamentoRepository;
+import com.example.algamoney.api.repository.PessoaRepository;
+import com.example.algamoney.api.service.exception.PessoaInexistenteOuInativaException;
 
 @Service
 public class LancamentoService {
 	
 	@Autowired
-	public LancamentoRepository lancamentoRepository;
+	private PessoaRepository pessoaRepository;
+	
+	@Autowired 
+	private LancamentoRepository lancamentoRepository;
 	
 	public Lancamento atualizar(Long codigo, Lancamento lancamento) {
 		Lancamento lancamentoSalvo = buscarLancamentoPeloCodigo(codigo);
@@ -27,6 +33,15 @@ public class LancamentoService {
 			throw new EmptyResultDataAccessException(1);
 		}
 		return lancamentoSalvo;
+	}
+
+	public Lancamento salvar(Lancamento lancamento) {
+		Pessoa pessoa = pessoaRepository.findOne(lancamento.getPessoa().getCodigo());
+		if (pessoa == null || pessoa.isInativo()) {
+			throw new PessoaInexistenteOuInativaException();
+		}
+		
+		return lancamentoRepository.save(lancamento);
 	}
 
 }
